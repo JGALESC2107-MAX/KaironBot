bd5941028d345feeb7d892f043110f3575721dae
JGALESC2107-MAX <juanantoniogalianescabias@gmail.com>
2026-06-30 22:57:56 +0200

    Update bot name from 'Kairon Group' to 'Kairon Bot'

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Kairon Group",
+        name: "Kairon Bot",
         // Activity type number (0 = Playing).
         type: 2,
       },
